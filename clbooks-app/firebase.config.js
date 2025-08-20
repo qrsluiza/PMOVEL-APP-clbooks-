74c6ec9ed4baac13a2b4256a78576fbe806368dc
qrsluiza <159432484+qrsluiza@users.noreply.github.com>
2025-08-20 08:30:38 -0300

    update

---
+++ clbooks-app/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsehGfZnZKtSGLDdg_mxY9ICrPudgoeQM",
+  authDomain: "clbooks-app.firebaseapp.com",
+  projectId: "clbooks-app",
+  storageBucket: "clbooks-app.firebasestorage.app",
+  messagingSenderId: "1061411260349",
+  appId: "1:1061411260349:web:bc29b741959cc87937f4d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
